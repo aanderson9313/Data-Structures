9c1e435a31e398b591c0b0db23c58976c030de88
alexis-anderson <aanderson9313@gmail.com>
2020-09-24 21:08:15 -0400

    finished mvp day 3

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,20 +17,85 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        #check if value is int
+        value_type = type(value)
+        if value_type != int and value_type != float:
+            print (f"Error: Insert type is {value_type}")
+            print ("Type must be 'int'")
+            return
+        #check if the value is < the value of the current node's value
+        if value < self.value:
+            #if there's no left child already there
+            if not self.left:
+                # add new node to the left
+                left_node = BSTNode(value)
+                #create a BSTNode and encapsulate the value in it and then set it to the Left Mode
+                self.left = left_node
+                #otherwise recursively call insert on the left node
+            else:
+                self.left.insert(value)
+            #otherwise recursively call insert on the right node
+            else: 
+                self.right.insert(value)
+            
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        #if the value of the current node matches the target
+        if target == self.value:
+            #return True
+            return True
+        #check if the target is < the value of the current nodes value
+        elif target < self.value:
+            # if theres no left child already there
+            if not self.left:
+                #return False
+                return False
+            #otherwise
+            else: 
+                #return a call of 'contains' on the Left child passing in the target value
+                return self.left.contains(target)
+            #otherwise the target is > the value of the current node
+            elif target > self.value:
+                #if theres no Right child already there
+                if not self.right:
+                    #return False
+                    return False
+                #otherwise
+                else:
+                    #return call of 'contains' on the right child passing in the target value
+                    return self.right.contains(target)
+                else: print(f"could not locate {target} in tree")
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        #check for empty tree
+        if not self.value:
+            #return None
+            print('tree is empty')
+            return None
+        
+        #check if there is node to the right
+        if not self.right:
+            #if true return value
+            return self.value
+        #otherwise return call to get_max on the right child
+        else:
+            return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        #call function passing in the current nodes value
+        fn(self.value)
+        #if there is a node to the left
+        if self.left:
+            #call the fn on the left value
+            self.left.for_each(fn)
+        #if there is a node on the right
+        else:
+            #call fn on the right value
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
